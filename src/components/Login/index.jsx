f25b049774442f8a0783f7e6b8c81bb530ace2ed
mbfreitas0 <mbfreitas0@gmail.com>
2022-09-01 16:29:56 -0300

    commit-01092022-1630

---
+++ src/components/Login/index.jsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import styles from "./styles.module.css";
+
+const Login = () => {
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState("");
+
+  const handleChange = ({ currentTarget: input }) => {
+    setData({ ...data, [input.name]: input.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const url = `http://localhost:5000/auth`;
+      const { data: res } = await axios.post(url, data);
+      localStorage.setItem("token", res.data);
+      window.location = "/";
+      console.log(res.message);
+    } catch (erro) {
+      if (
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status <= 500
+      ) {
+        setError(error.response.data.message);
+      }
+    }
+  };
+  return (
+    <div className={styles.login_container}>
+      <div className={styles.login_form_container}>
+        <div className={styles.left}>
+          <form className={styles.form_container} onSubmit={handleSubmit}>
+            <h1>Login to your Account</h1>
+
+            <input
+              type="text"
+              placeholder="Email"
+              name="email"
+              onChange={handleChange}
+              value={data.email}
+              required
+              className={styles.input}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={handleChange}
+              value={data.password}
+              required
+              className={styles.input}
+            />
+            {error && <div className={styles.error_msg}> {error}</div>}
+            <button type="submit" className={styles.gree_btn}>
+              Sign In
+            </button>
+          </form>
+          <div className={styles.right}></div>
+          <div className={styles.right}>
+            <h1>New Here ?</h1>
+            <Link to="/signup">
+              <button type="button" className={styles.white_btn}>
+                Sign Up
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
