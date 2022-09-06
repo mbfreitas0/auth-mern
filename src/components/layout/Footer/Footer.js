d10aaf4fbea21221f9c3a79d05ee879150775e49
mbfreitas0 <mbfreitas0@gmail.com>
2022-09-06 17:24:34 -0300

    commit-06092022-1725

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -1,11 +1,25 @@
 import React from "react";
-import styles from "../Footer/styles.module.css";
+import styles from "../Footer/Footer.module.css";
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div class={styles.footer}>
-      <p>Cata-Vento &copy; 2022</p>
-    </div>
+    <footer className={styles.footer}>
+      <ul className={styles.social_list}>
+        <li>
+          <FaFacebook />
+        </li>
+        <li>
+          <FaInstagram />
+        </li>
+        <li>
+          <FaLinkedin />
+        </li>
+      </ul>
+      <p className={styles.copy_right}>
+        <span>Cata-Vento</span> &copy; 2022
+      </p>
+    </footer>
   );
 };
 
