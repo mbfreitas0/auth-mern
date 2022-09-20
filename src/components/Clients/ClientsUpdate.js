de94b35563c4c2939899d503378347755e5b3088
mbfreitas0 <mbfreitas0@gmail.com>
2022-09-20 20:47:56 -0300

    commit-20092022-2048

--- src/components/Clients/ClientsUpdate.js
+++ src/components/Clients/ClientsUpdate.js
@@ -1,37 +1,116 @@
 import React from "react";
-import styles from "../Clients/ClientsUpdate.module.css";
-import { useParams } from "react-router-dom";
+
+// import styles from "../Clients/ClientsUpdate.module.css";
+import { useNavigate, useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
-import Loading from "../../components/layout/Loading";
-import Container from "../layout/Container/Container";
+// import Loading from "../../components/layout/Loading";
+// import Container from "../layout/Container/Container";
+
+const client = {
+  firstName: "",
+  lastName: "",
+  tel: "",
+  address: "",
+  number: "",
+};
 
 const ClientsUpdate = () => {
   const { id } = useParams();
+  // console.log(id);
+  const [values, setValues] = useState(client);
+  // const [showClientForm, setShowClientForm] = useState(false);
+
+  const navigate = useNavigate();
   console.log(id);
-  const [client, setClient] = useState([]);
-  const [showClientForm, setShowClientForm] = useState(false);
 
   useEffect(() => {
-    setTimeout(() => {
-      fetch(`http://localhost:5000/clients/${id}`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
+    fetch(`http://localhost:5000/clients/${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setValues(data);
+        console.log(data);
       })
-        .then((resp) => resp.json())
-        .then((data) => {
-          setClient(data);
-        })
-        .catch((err) => console.log(err));
-    }, 500);
+      .catch((err) => console.log(err));
   }, [id]);
 
-  const toggleClientForm = () => {
-    setShowClientForm(!showClientForm);
+  const onValueChange = (e) => {
+    this.setValues({ ...values, [e.target.name]: e.target.value });
   };
 
-  return <>{client.firstName ? client.firstName : <Loading />}</>;
+  function onSubmit(e) {
+    e.preventDefault();
+    navigate("/client-list");
+  }
+
+  // const toggleClientForm = () => {
+  //   setShowClientForm(!showClientForm);
+  // };
+
+  return (
+    <div>
+      <h1>Edição de Clientes</h1>
+      <form onSubmit={onSubmit}>
+        <div className="promotion-form__group">
+          <label htmlFor="firstName">Nome</label>
+          <input
+            id="firstName"
+            name="firstName"
+            type="text"
+            onChange={(e) => onValueChange(e)}
+            value={values.firstName}
+          />
+        </div>
+        <div className="promotion-form__group">
+          <label htmlFor="lastName">Sobrenome</label>
+          <input
+            id="lastName"
+            name="lastName"
+            type="text"
+            onChange={(e) => onValueChange(e)}
+            value={values.lastName}
+          />
+        </div>
+        <div className="promotion-form__group">
+          <label htmlFor="tel">tel</label>
+          <input
+            id="tel"
+            name="tel"
+            type="text"
+            onChange={(e) => onValueChange(e)}
+            value={values.tel}
+          />
+        </div>
+        <div className="promotion-form__group">
+          <label htmlFor="address">Endereço</label>
+          <input
+            id="address"
+            name="address"
+            type="text"
+            onChange={(e) => onValueChange(e)}
+            value={values.address}
+          />
+        </div>
+        <div className="promotion-form__group">
+          <label htmlFor="number">Número</label>
+          <input
+            id="number"
+            name="number"
+            type="text"
+            onChange={(e) => onValueChange(e)}
+            value={values.number}
+          />
+        </div>
+        <div>
+          <button type="submit">Salvar</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default ClientsUpdate;
